b15b55abe8aa0ac4271af8e577e89c46b65bf86c
stay_miku <staymiku520@gmail.com>
2023-03-30 21:36:18 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pbrm',
+    version='0.0.1',
+    description='A pixiv bookmarks local backup tool',
+    author='stay_miku',
+    author_email='none',
+    url='https://github.com/yourusername/myproject',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
