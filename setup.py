d1d75f8771631c1948660707aef47926f3fcdaee
stay_miku <staymiku520@gmail.com>
2023-03-31 21:35:03 +0800

    修bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbrm',
-    version='0.0.2',
+    version='0.0.3',
     description='A pixiv bookmarks local backup tool',
     author='stay_miku',
     author_email='root@staymiku.top',
