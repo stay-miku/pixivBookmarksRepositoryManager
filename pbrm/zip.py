c66a9e1c10929f742201d2c650760c0d58c52e1d
stay_miku <staymiku520@gmail.com>
2023-08-14 18:04:58 +0900

    新增zip打包

--- pbrm/zip.py
+++ pbrm/zip.py
@@ -1,16 +1,23 @@
 import zipfile
 from . import config
 import os
+import shutil
 
 
 def zip_out(path: str):
-    with zipfile.ZipFile(path, "w") as f:
-        i = 0
+    with zipfile.ZipFile(path, "w", zipfile.ZIP_DEFLATED) as f:
+        i = 1
+        j = 1
         all_illusts = [f for f in os.listdir(config.repository_path) if os.path.isdir(os.path.join(config.repository_path, f))]
         for illust in all_illusts:
             pic = [f for f in os.listdir(os.path.join(config.repository_path, illust)) if len(f.lower().split(".")) >= 2 and f.lower().split(".")[1] != "json"]
+            m = 1
             for p in pic:
-                print("{} : {}".format(i, os.path.join(config.repository_path, illust, p)))
-                f.write(os.path.join(config.repository_path, illust, p))
+                print("{}-{} {} : {}".format(j, m, i, os.path.join(config.repository_path, illust, p)))
+                base_name = os.path.basename(os.path.join(config.repository_path, illust, p))
+                f.write(os.path.join(config.repository_path, illust, p), base_name)
+                i += 1
+                m += 1
+            j += 1
 
 
